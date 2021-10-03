8f9646f0278dd1532e390f26065ef11243034265
HystERICKal <nyoroerick12@gmail.com>
2021-10-04 00:46:17 +0200

    Completed Admin Dashboard

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar"
 import Home from "./pages/home/Home"
 import "./app.css"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import UserList from "./pages/userList/UserList";
+import User from "./pages/user/User";
+import NewUser from "./pages/newUser/NewUser";
+import Product from "./pages/product/Product";
+import ProductList from "./pages/productList/ProductList";
+import NewProduct from "./pages/newProduct/NewProduct";
 
 function App() {
   return (
-    <div >
+    <Router >
 
       <Topbar />
 
@@ -14,13 +21,42 @@ function App() {
 
         <Sidebar/>
 
-        <Home />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+
+          <Route path="/users">
+            <UserList/>
+          </Route>
+
+          <Route path="/user/:userId">
+            <User />
+          </Route>
+
+          <Route path="/newUser">
+            <NewUser />
+          </Route>
+
+          <Route path="/products">
+            <ProductList />
+          </Route>
+
+          <Route path="/product/:productId">
+            <Product />
+          </Route>
+
+          <Route path="/newproduct">
+            <NewProduct />
+          </Route>
+
+        </Switch>
         
       </div>
 
       
 
-    </div>
+    </Router>
   );
 }
 
